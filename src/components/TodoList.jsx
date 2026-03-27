dd69df6d5ef4d4309964b4a8fbc4931d7f5681e7
Florencia Navarro <cecine_flor@hotmail.com>
2026-03-27 09:34:56 -0300

    proyecto terminado

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,59 +9,74 @@ function TodoList ({tareas, eliminarTarea, toggleComplete, editandoId, nuevoText
         <AnimatePresence>
             {tareas.map((tarea) => (
                 <motion.li
-                 key={tarea.id}
-                 initial={{opacity: 0, y: -10}}
-                 animate={{opacity: 1, y: 0}}
-                 exit={{ opacity: 0, x: 50}}
-                 transition={{ duration: 0.3 }}
-                >
+                    key={tarea.id}
+                    initial={{opacity: 0, y: -10}}
+                    animate={{opacity: 1, y: 0}}
+                    exit={{ opacity: 0, x: 50}}
+                    transition={{ duration: 0.3 }}
+                    >
+
+                    {editandoId === tarea.id ? (
+                    <div className="tarea-editando">
+                        <input
+                            value={nuevoTexto}
+                            onChange={(e) => setNuevoTexto(e.target.value)}
+                            onKeyDown={(e) => {
+                            if (e.key === "Enter"){
+                                guardarEdicion(tarea.id)
+                            }
+                            }}
+                        />  
+
+                        <div className="botones-edicion">
+                            <motion.button 
+                                onClick={() => guardarEdicion(tarea.id)} 
+                                className="btn-guardar"
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{ scale: 1.05 }}
+                            >
+                                Guardar
+                            </motion.button>
+
+                            <motion.button 
+                                onClick={cancelarEdicion} className="btn-cancelar"
+                                whileTap={{ scale: 0.9 }}
+                                whileHover={{ scale: 1.05 }}
+                            >
+                                ❌
+                            </motion.button>
+
+                        </div>
+                    </div>
+
+          ) : (
                     <div className="tarea-info">
                         <input
                             type="checkbox"
                             checked={tarea.completed}
                             onChange={() => toggleComplete(tarea.id)}
                         />
-                        {editandoId === tarea.id ? (
-                            <input 
-                            value={nuevoTexto}
-                            onChange={(e) => setNuevoTexto(e.target.value)}
-                            onKeyDown={(e) => {
-                                if (e.key === "Enter"){
-                                    guardarEdicion(tarea.id)
-                                }
-                            }}
-                        />
-                        ) : (
-                            <span 
+
+                        <span 
                             onClick={() => empezarEdicion(tarea)}
                             className={tarea.completed ? "tarea-completa" : ""}
                             style={{ cursor: "pointer"}}
-                            >
-                                {tarea.texto}
-                            </span>
-                        )}
-                        {/* <span style={{ textDecoration: tarea.completed ? "line-through" : "none" }}>
+                        >
                             {tarea.texto}
-                        </span> */}
+                        </span>
 
+                        <motion.button 
+                            onClick={() => eliminarTarea(tarea.id)} className="btn-eliminar"
+                            whileTap={{ scale: 0.9 }}
+                            whileHover={{ scale: 1.05 }}    
+                        >
+                            Eliminar
+                        </motion.button>
                     </div>
-                    
-                    <button onClick={() => eliminarTarea(tarea.id)}>
-                        Eliminar
-                    </button>
-                    
-                    {editandoId === tarea.id && (
-                        <>
-                        <button onClick={() => guardarEdicion(tarea.id)}> 
-                            Guardar
-                        </button>
-                        <button onClick={cancelarEdicion} style={{marginLeft: "5px"}}>
-                            ❌
-                        </button>
-                        </>
-                    )}
+      )}
+
                 </motion.li>
-            ))}
+  ))}
         </AnimatePresence>
     )
 }
